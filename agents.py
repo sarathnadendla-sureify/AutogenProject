ddd9a4d586a3d66c805ff841b983bc1eadc74006
sarathnadendla <sarath@sureify.com>
2025-02-17 21:50:23 +0530

    code files regarding autoden initial commit.

---
+++ agents.py
@@ -0,0 +1,9 @@
+import os
+from autogen import AssistantAgent
+from dotenv import load_dotenv
+load_dotenv()
+
+# os.environ[""]=os.getenv('')
+
+class ResearchAgent():
+    pass
---
+++ data_loader.py
@@ -0,0 +1,9 @@
+import requests
+import xml.etree.ElementTree as ET
+from scholarly import scholarly
+
+print("OK")
+
+
+class DataLoader:
+    pass
